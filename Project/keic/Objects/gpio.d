faeb6453f611ef14d93d5b4d54a60a1d8152e66f
SonRiGard <luongvansonqnvn@gmail.com>
2021-06-27 17:46:14 +0300

    Revert "ok"
    This reverts commit ab82914f501df10516f9873481ae89e949934f62.

--- Project/keic/Objects/gpio.d
+++
@@ -1,6 +0,0 @@
-.\objects\gpio.o: GPIO.c
-.\objects\gpio.o: GPIO.h
-.\objects\gpio.o: C:\Users\heozu\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\Include\stm32f10x.h
-.\objects\gpio.o: ..\..\Lib\CMSIS\CM3\CoreSupport\core_cm3.h
-.\objects\gpio.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\gpio.o: C:\Users\heozu\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\Include\system_stm32f10x.h
--- Project/keic/Objects/tim2.d
+++
@@ -1,6 +0,0 @@
-.\objects\tim2.o: TIM2.c
-.\objects\tim2.o: TIM2.h
-.\objects\tim2.o: C:\Users\heozu\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\Include\stm32f10x.h
-.\objects\tim2.o: ..\..\Lib\CMSIS\CM3\CoreSupport\core_cm3.h
-.\objects\tim2.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\tim2.o: C:\Users\heozu\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\Include\system_stm32f10x.h
