ab82914f501df10516f9873481ae89e949934f62
SonRiGard <luongvansonqnvn@gmail.com>
2021-06-27 17:24:48 +0300

    ok

---
+++ Project/keic/Objects/spi.d
@@ -0,0 +1,6 @@
+.\objects\spi.o: SPI.c
+.\objects\spi.o: SPI.h
+.\objects\spi.o: C:\Users\heozu\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\Include\stm32f10x.h
+.\objects\spi.o: ..\..\Lib\CMSIS\CM3\CoreSupport\core_cm3.h
+.\objects\spi.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\spi.o: C:\Users\heozu\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\Include\system_stm32f10x.h
